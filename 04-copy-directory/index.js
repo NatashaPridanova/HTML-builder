70c5b6c120bfbe952649f7127fff4a9145ae84a8
Natallia <natashapridanova17@gmail.com>
2022-05-22 09:04:10 +0300

    feat: write a solution to the fourth task (Sun, May 22, 2022 09:04:09 AM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,44 @@
+//реализуйте функцию copyDir которая копирует содержимое папки files в папку files-copy
+const fs = require('fs');
+const path = require('path');
 
+function copyDir() {
+    initCopying();
+
+    fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+        if (err) throw err;
+        const filesToExist = [];
+        for (const file of files) {
+            filesToExist.push(file);
+            fs.writeFile(
+                path.join(__dirname, 'files-copy', file),
+                '',
+                (err) => {
+                    if (err) throw err;
+                }
+            );
+            fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
+                if (err) throw err;
+                for (const file of files) {
+                    if (filesToExist.indexOf(file) < 0) {
+                        fs.unlink(path.join(__dirname, 'files-copy', file), err => {});
+                    }
+                }
+            });
+            fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => {
+                if (err) throw err;
+            })
+        }
+    });
+}
+copyDir();
+
+function initCopying() {
+    fs.access(path.join(__dirname, 'files-copy'), fs.F_OK, (err) => {
+        if (err) {
+            fs.mkdir(path.join(__dirname, 'files-copy'), err => {
+                if (err) throw err;
+            });
+        }
+    })
+}
